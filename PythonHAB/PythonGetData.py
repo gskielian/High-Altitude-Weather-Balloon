a04f88f437d2f582e188e61b1ee6fe48605a9393
gskielian <gregory.kielian@hackerdojo.com>
2013-07-25 07:29:50 -0700

    Updated the script to work with Raspberry Pi (will need to change path to python if Mac)
    If mac will need to change path to python and the serial port.

--- PythonHAB/PythonGetData.py
+++ PythonHAB/PythonGetData.py
@@ -1,8 +1,9 @@
-#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+#!/usr/bin/python
 
 import serial
 
-ser = serial.Serial('/dev/tty.usbmodem1421',115200)
+ser = serial.Serial('/dev/ttyACM0',115200)
+
 f = open('dataFile.txt','a')
 
 while 1 :
