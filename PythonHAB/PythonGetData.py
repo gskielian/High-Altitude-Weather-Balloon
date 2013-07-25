aef3940ef6237c6aac71fb905d7f1f7d82d1ee2f
gskielian <gregory.kielian@hackerdojo.com>
2013-07-24 19:59:30 -0700

    added stuff

---
+++ PythonHAB/PythonGetData.py
@@ -0,0 +1,14 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
+import serial
+
+
+ser = serial.Serial('/dev/tty.usbmodem1421',115200)
+f = open('dataFile.txt','a')
+
+while 1 :
+    f.write(ser.readline())
+    f.close()
+    f = open('dataFile.txt','a')
+
+
